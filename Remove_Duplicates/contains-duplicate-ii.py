68398b591a299da53ad69a4bcad5b5ef6954c112
unknown <sthitpanth01@gmail.com>
2022-02-18 00:45:45 +0600

    Add another solution

---
+++ Remove_Duplicates/contains-duplicate-ii.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def containsDuplicates(self, List:List[int], k:int):
+        length = len(List) # dont do len(List) - 1, range will take care of it
+
+        hashMap = {}
+        for index in range(length): # range will use length to create number 0 to length -1 
+            key = List[index]
+            if key in hashMap:
+                hashMap[key].append(index)
+                hash_map_length = len(hashMap[key])
+                hash_last_value = hashMap[key][hash_map_length - 1]
+                hash_second_last_value = hashMap[key][hash_map_length - 2]
+                if (hash_last_value - hash_second_last_value) <= k: 
+                    return True
+            else:
+                hashMap[key] = [index]
+
+        return False
+    def containsNearbyDuplicate(self, List:List[int], k:int):
+        length = len(List) # dont do len(List) - 1, range will take care of it
+ 
+        hashMap = {}
+        for index in range(length): # range will use length to create number 0 to length -1 
+            key = List[index]
+            if key in hashMap and (index - hashMap[key]) <= k: 
+                return True
+            hashMap[key] = index
+
+        return False
+    
+ans = Solution()
+print(ans.containsNearbyDuplicate([3,3], 2))
---
+++ Remove_Duplicates/contains-duplicate-iii.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        pass
+        
