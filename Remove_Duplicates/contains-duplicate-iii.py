d2415e1b65461299c8c7be528edfcb55963c4216
unknown <sthitpanth01@gmail.com>
2022-02-24 21:39:45 +0600

    add comments to contains-duplicate-iii

--- Remove_Duplicates/contains-duplicate-iii.py
+++ Remove_Duplicates/contains-duplicate-iii.py
@@ -5,17 +5,25 @@ class Solution:
     def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
         length = len(nums)
         cache = []
+        # we are avoiding using object caching
+        # store the number and its index in two dimensional array eg - [[2,1],[1,2]]
         for index in range(length):
             cache.append((nums[index], index))
         
+        # 2D array cache is sorted
         cache = sorted(cache, key=lambda x: x[0])
-        print(cache)
 
+        # loop till before the last element as i+1 is used
+        # now it can check two numbers iteratively to see if the difference is less than or equal to t
+        # if so they are nearby duplicates but now indexes need to be checked
+        # if they are nearby duplicates and also index diff is <= k then true is returned
         for i in range(length-1):
             if abs(cache[i][0] - cache[i+1][0]) <= t and abs(cache[i][1] - cache[i+1][1]) <= k:
                 return True
 
         return False
+
+    # Need to find a O(N) solution
 
 ans = Solution()
 print(ans.containsNearbyAlmostDuplicate([3,3,1], 1, 1))
